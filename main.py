87ce75095920350d0af20ebd154ecc28fc12d0d0
jckane5187 <jckane5187@gmail.com>
2026-02-13 21:48:16 -0600

    added functionality to monitor the token count of each response

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ if api_key == None:
 client = genai.Client(api_key=api_key)
 model = "gemini-2.5-flash"
 response = client.models.generate_content(model=model, contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+if response.usage_metadata == None:
+    raise RuntimeError("failed API request detected")
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(response.text)
+
 
 def main():
     print("Hello from aiagent!")
