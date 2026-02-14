f18e3b6259f91e90e2ddfa7424d56384f65236f4
jckane5187 <jckane5187@gmail.com>
2026-02-13 20:59:44 -0600

    finally got the api key to load. updated gitignore. simply loaded the api key and hardcoded a question to the api

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key == None:
+    raise RuntimeError("no api key found")
+
+client = genai.Client(api_key=api_key)
+model = "gemini-2.5-flash"
+response = client.models.generate_content(model=model, contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+print(response.text)
+
 def main():
     print("Hello from aiagent!")
 
