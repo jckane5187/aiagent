59596db978a5a8842707380f61e08f0f22348563
jckane5187 <jckane5187@gmail.com>
2026-02-14 16:47:33 -0600

    imported the argparse module to allow me to creat new arguments. created a verbose argument that includes additional states while also greating an arg for the prompt taken at run time. finally, created a message storing capability using types

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,13 +10,24 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise RuntimeError("no api key found")
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+user_prompt = args.user_prompt
+
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
 client = genai.Client(api_key=api_key)
 model = "gemini-2.5-flash"
-response = client.models.generate_content(model=model, contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+response = client.models.generate_content(model=model, contents=messages)
 if response.usage_metadata == None:
     raise RuntimeError("failed API request detected")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if args.verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(response.text)
 
 
