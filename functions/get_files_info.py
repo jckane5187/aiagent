fc94c85ad4b402cab13b9644a86a51cc6eae1ed8
jckane5187 <jckane5187@gmail.com>
2026-02-15 20:08:20 -0600

    created two new directories to house specific components for testing and for the agent to work on. created a get files function and tested it

---
+++ functions/get_files_info.py
@@ -0,0 +1,25 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+    # Will be True or False
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    if not valid_target_dir:
+        return f"Error: Cannot list {directory} as it is outside the permitted working directory"
+    if not os.path.isdir(target_dir):
+        return f"Error: {directory} is not a directory"
+    
+    results = ["Results for current directory:"]
+
+    for item in os.listdir(target_dir):
+        fullpath = os.path.join(target_dir, item)
+        name = item
+        file_size = os.path.getsize(fullpath)
+        is_dir = os.path.isdir(fullpath)
+        results.append(f"\n- {name}: file_size={file_size} bytes, is_dir={is_dir}")
+    
+    final_string = "".join(results)
+    return final_string
+
+
